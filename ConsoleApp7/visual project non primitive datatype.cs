15ea1c9036be150b126f89d2f0a3379aaf5486e7
nimra <nimrarafiq02@gmail.com>
2021-06-07 16:13:47 +0500

    Add project files.

---
+++ ConsoleApp7/visual project non primitive datatype.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharpFundamentals
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+
+        }
+]}   }
